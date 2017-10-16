4f1f5681b2a7dae7724abdabab6845f22deeda09
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-16 12:03:11 -0700

    Working nav loggedin button.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,17 @@ class SessionsController < ApplicationController
 
     if @user
       session[:user_id] = @user.id
-        flash[:success] = "Welcome Tryingthisoutagain"
+        flash[:success] = "Welcome back #{@login_user.name}"
     else
       @user = User.new(uid: @auth_hash['uid'], provider: @auth_hash['provider'], name: @auth_hash['info']['name'], username: @auth_hash['info']['nickname'], email: @auth_hash['info']['email'])
         if @user.save
           session[:user_id] = @user.id
-          flash[:success] = "Welcome Tryingthisout"
+          flash[:success] = "Welcome to MediaRanker, #{@login_user.name}"
         else
           flash[:error] = "Unable to save user!"
         end
     end
     redirect_to root_path
-
   end
 
   def index
