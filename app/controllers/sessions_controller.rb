44bc3d49bf1c8b8fe1abd447857c0d01e943ca29
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-16 11:56:52 -0700

    Additional username field for user model, working sessions create method.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,38 +1,26 @@
 class SessionsController < ApplicationController
-  def login_form
+  def create
+    @auth_hash = request.env['omniauth.auth']
+
+    @user = User.find_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
+
+    if @user
+      session[:user_id] = @user.id
+        flash[:success] = "Welcome Tryingthisoutagain"
+    else
+      @user = User.new(uid: @auth_hash['uid'], provider: @auth_hash['provider'], name: @auth_hash['info']['name'], username: @auth_hash['info']['nickname'], email: @auth_hash['info']['email'])
+        if @user.save
+          session[:user_id] = @user.id
+          flash[:success] = "Welcome Tryingthisout"
+        else
+          flash[:error] = "Unable to save user!"
+        end
+    end
+    redirect_to root_path
+
   end
 
-  def create
-   auth_hash = request.env['omniauth.auth']
-   raise
+  def index
+    @user = User.find(session[:user_id]) # < recalls the value set in a previous request
   end
-  # def login
-  #   username = params[:username]
-  #   if username and user = User.find_by(username: username)
-  #     session[:user_id] = user.id
-  #     flash[:status] = :success
-  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-  #   else
-  #     user = User.new(username: username)
-  #     if user.save
-  #       session[:user_id] = user.id
-  #       flash[:status] = :success
-  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-  #     else
-  #       flash.now[:status] = :failure
-  #       flash.now[:result_text] = "Could not log in"
-  #       flash.now[:messages] = user.errors.messages
-  #       render "login_form", status: :bad_request
-  #       return
-  #     end
-  #   end
-  #   redirect_to root_path
-  # end
-  #
-  # def logout
-  #   session[:user_id] = nil
-  #   flash[:status] = :success
-  #   flash[:result_text] = "Successfully logged out"
-  #   redirect_to root_path
-  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407164321) do
+ActiveRecord::Schema.define(version: 20171016185248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "email"
+    t.integer  "uid",        null: false
+    t.string   "provider",   null: false
+    t.string   "username"
   end
 
   create_table "votes", force: :cascade do |t|
