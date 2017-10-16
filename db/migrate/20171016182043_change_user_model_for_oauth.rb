353796582bc9f5b93b1960a6a615bb904ab0c0fd
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-10-16 11:25:34 -0700

    Migration for users table.

---
+++ db/migrate/20171016182043_change_user_model_for_oauth.rb
@@ -0,0 +1,9 @@
+class ChangeUserModelForOauth < ActiveRecord::Migration[5.0]
+  def change
+    add_column(:users, :name, :string)
+    add_column(:users, :email, :string)
+    add_column(:users, :uid, :integer, null: false)
+    add_column(:users, :provider, :string, null: false)
+    remove_column(:users, :username)
+  end
+end
